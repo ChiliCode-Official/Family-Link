1201319f490139f4cf0b7d6a127c47754e5886c1
ChiliCode <tilinrodrigo3@gmail.com>
2026-08-16 15:42:06 -0600

    Configure base path for asset routing
    Set base path for relative asset routing in build.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // IMPORTANTE: Esto asegura que al hacer el "build", 
+  // los assets (js, css, imágenes) usen rutas relativas y no absolutas.
+  base: './', 
 })
